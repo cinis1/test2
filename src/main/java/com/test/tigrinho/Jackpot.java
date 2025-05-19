2a49e4cc2eb50bd049374265569869ab99399336
guilherme <schneidercguilherme@gmail.com>
2025-05-18 23:43:49 -0300

    test

--- src/main/java/com/test/tigrinho/Jackpot.java
+++ src/main/java/com/test/tigrinho/Jackpot.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Jackpot {
     @GetMapping(path = "/jackpot")
     public static String jackpot (){
-        return "jackpot!";
+        return "Jackpot!";
     }
 
 }
